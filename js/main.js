a9b88d12bedd6c7a5ecf07cb7bb836afdae366ba
bertakang <bertakang@gmail.com>
2024-09-29 02:22:30 +0700

    updates

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 var dropdown = document.querySelector('.dropdown');
 var dropdownContent = document.querySelector('.dropdown-content'); // Move this outside
-var serviceContainers = document.querySelectorAll('.service-container'); 
-var serviceCard = document.querySelector('.service-card');
-var serviceCardImage = document.querySelector('.service-card-img');
+
 
 dropdown.addEventListener('mouseenter', function() {
     if (dropdownContent) {
@@ -11,25 +9,6 @@ dropdown.addEventListener('mouseenter', function() {
     
 });
 
-serviceContainers.forEach(container => {
-    var serviceCard = container.querySelector('.service-card');
-    var serviceCardImage = container.querySelector('.service-card-img');
-
-    container.addEventListener('mouseenter', function() {
-        serviceCard.style.color = "aliceblue";
-        serviceCard.style.display = "flex";
-        serviceCard.style.textShadow = "2px 4px 6px rgba(0, 0, 0, 0.5)";
-        serviceCardImage.style.display = "flex";
-        serviceCardImage.style.position = "absolute";
-    });
-
-    container.addEventListener('mouseleave', function() {
-        serviceCard.style.color = "black";
-        serviceCardImage.style.display = "none";
-        serviceCard.style.textShadow = "none";
-    });
-
-});
 
 
 // Close dropdown if clicking outside
@@ -40,5 +19,3 @@ document.addEventListener('click', function(event) {
         }
     }
 });
-
-anime.stagger(serviceCard, {grid: grid, from: index})
